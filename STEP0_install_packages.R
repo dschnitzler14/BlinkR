a338664866520b61f14648a88289b6d19bf7802f
Danny <dschnitzler@outlook.com>
2025-03-25 16:44:22 +0000

    fix readlines warning

---
+++ STEP0_install_packages.R
@@ -0,0 +1,47 @@
+required_packages <- c(
+  "shiny",
+  "bslib",
+  "shinydashboard",
+  "markdown",
+  "DT",
+  "shinyAce",
+  "shinyauthr",
+  "dplyr",
+  "ggplot2",
+  "car",
+  "tidyr",
+  "utils",
+  "googlesheets4",
+  "googledrive",
+  "readr",
+  "markdownInput",
+  "knitr",
+  "shinycssloaders",
+  "future",
+  "promises",
+  "shinyWidgets",
+  "tibble",
+  "stringr",
+  "shinyjs",
+  "jsonlite",
+  "datasets",
+  "evaluate",
+  "rstatix",
+  "coin",
+  "rsconnect",
+  "cookies",
+  "whisker",
+  "sortable",
+  "grid",
+  "png",
+  "gridExtra"
+)
+
+installed_packages <- rownames(installed.packages())
+missing_packages <- setdiff(required_packages, installed_packages)
+
+if (length(missing_packages)) {
+  install.packages(missing_packages)
+}
+
+lapply(required_packages, library, character.only = TRUE)
